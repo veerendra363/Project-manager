6fb55c85f0fa3d31ed306c4b9b9ac444c88032dd
veerendra363 <veeruparadesi363@gmail.com>
2025-08-20 12:38:46 +0530

    Updated the queries to get single object response

--- api/schema/queries.py
+++ api/schema/queries.py
@@ -1,5 +1,5 @@
 import graphene
-from .types import OrganizationListType, ProjectListType, TaskCommentListType, TaskListType
+from .types import OrganizationListType, OrganizationType, ProjectListType, ProjectType, TaskCommentListType, TaskListType, TaskType
 from api.models import Organization, Project, Task, TaskComment
 
 class Query(graphene.ObjectType):
@@ -10,6 +10,7 @@ class Query(graphene.ObjectType):
         page_size=graphene.Int(),
         order_by=graphene.String(default_value="desc")  # new argument
     )
+    organization = graphene.Field(OrganizationType, id=graphene.ID(required=True))
 
     # Projects
     projects = graphene.Field(
@@ -19,6 +20,7 @@ class Query(graphene.ObjectType):
         page_size=graphene.Int(),
         order_by=graphene.String(default_value="desc")
     )
+    project = graphene.Field(ProjectType, id=graphene.ID(required=True))
 
     # Tasks
     tasks = graphene.Field(
@@ -28,6 +30,7 @@ class Query(graphene.ObjectType):
         page_size=graphene.Int(),
         order_by=graphene.String(default_value="desc")
     )
+    task = graphene.Field(TaskType, id=graphene.ID(required=True))
 
     # Task Comments
     task_comments = graphene.Field(
@@ -44,6 +47,12 @@ class Query(graphene.ObjectType):
         start = (page - 1) * page_size
         end = start + page_size
         return OrganizationListType(total_count=qs.count(), results=qs[start:end])
+    
+    def resolve_organization(self, info, id):
+        try:
+            return Organization.objects.get(id=id)
+        except Organization.DoesNotExist:
+            return None
 
     def resolve_projects(self, info, organization_id, page=1, page_size=10, order_by="desc"):
         ordering = "-id" if order_by.lower() == "desc" else "id"
@@ -52,6 +61,13 @@ class Query(graphene.ObjectType):
         start = (page - 1) * page_size
         end = start + page_size
         return ProjectListType(total_count=total, results=qs[start:end])
+    
+    def resolve_project(self, info, id):
+        try:
+            return Project.objects.get(id=id)
+        except Project.DoesNotExist:
+            return None
+
 
     def resolve_tasks(self, info, project_id, page=1, page_size=10, order_by="desc"):
         ordering = "-id" if order_by.lower() == "desc" else "id"
@@ -60,6 +76,12 @@ class Query(graphene.ObjectType):
         start = (page - 1) * page_size
         end = start + page_size
         return TaskListType(total_count=total, results=qs[start:end])
+    
+    def resolve_task(self, info, id):
+        try:
+            return Task.objects.get(id=id)
+        except Task.DoesNotExist:
+            return None
 
     def resolve_task_comments(self, info, task_id, page=1, page_size=10, order_by="desc"):
         ordering = "-id" if order_by.lower() == "desc" else "id"
