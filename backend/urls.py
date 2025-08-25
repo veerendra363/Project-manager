77d9562a04c684218cfacd7daf427ab6c0f025c7
veerendra363 <veereparadesi363@gmail.com>
2025-08-25 18:32:53 +0530

    updated the urls

--- backend/urls.py
+++ backend/urls.py
@@ -22,5 +22,5 @@ from api.schema.schema import schema
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
+    path('graphql', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
 ]
